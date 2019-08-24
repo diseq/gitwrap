a537d96f87c5968de28feef885de5df570b6fe2c
Dieter Schwarenthorer <dieter@schwarenthorer.at>
2019-08-24 12:39:40 +0200

    src

---
+++ GitWrap/PathConverter.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace GitWrap
+{
+    public class PathConverter
+    {
+        // Convert Windows style paths submitted to GitWrap to their Linux style equivalent
+        public static string convertPathFromWindowsToLinux(String path)
+        {
+            // Translate directory structure.
+            // Use regex to translate drive letters.
+            string pattern = @"(\D):\\";
+            string argstr = path;
+            foreach (Match match in Regex.Matches(path, pattern, RegexOptions.IgnoreCase))
+            {
+                string driveLetter = match.Groups[1].ToString();
+                argstr = Regex.Replace(path, pattern, Properties.Settings.Default.bashpath + driveLetter.ToLower() + "/");
+            }
+
+            // Convert Windows path to Linux style paths
+            argstr = argstr.Replace("\\", "/");
+            // Escape any whitespaces
+            argstr = argstr.Replace(" ", "\\ ");
+            // Escape parenthesis
+            argstr = argstr.Replace("(", "\\(");
+            argstr = argstr.Replace(")", "\\)");
+            return argstr;
+        }
+
+       // Convert output paths from WSL git to Windows style equivalent.
+       // For now we only convert forward slashes to backslashes if our regex pattern for
+       // absolute paths get a match. If not we will end up converting urls, and other stuff too.
+       public static string convertPathFromLinuxToWindows(String path) 
+        {
+            bool foundDrivePath = false;
+            string drivePathPattern = String.Format(@"{0}(\D)/", Properties.Settings.Default.bashpath);
+            string argstr = path;
+
+            foreach (Match match in Regex.Matches(path, drivePathPattern, RegexOptions.None))
+            {
+                string driveLetter = match.Groups[1].ToString();
+                argstr = Regex.Replace(path, drivePathPattern, driveLetter.ToUpper() + ":\\");
+                // Convert Linux style path separators to Windows style equivalent
+                argstr = argstr.Replace("/", "\\");
+                foundDrivePath = true;
+            }
+
+            if (foundDrivePath)
+            {
+                argstr = argstr.Replace("/", "\\");
+            }
+            return argstr;
+        }
+
+    }
+}
---
+++ GitWrap/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+
+namespace GitWrap
+{
+    class Program
+    {
+        private static int outputLines = 0;
+
+ 	static void Main(string[] args)
+        {
+          if (args.Length > 0 && args[0] == "--setBashPath" && args.Length == 2)
+          {
+              Properties.Settings.Default.bashpath = args[1];
+              Properties.Settings.Default.Save();
+              Console.Write("[*] bashPath set to: " + args[1]);
+          } else if (args.Length > 0 && args[0] == "--getBashPath") {
+              Console.Write("[*] Current bashPath: " + Properties.Settings.Default.bashpath);
+          }
+          else
+          {
+              executeGitWithArgs(Properties.Settings.Default.bashpath, args);
+          }
+        }
+        static void executeGitWithArgs(String bashPath, string[] args)
+        {
+
+            if (!File.Exists(bashPath))
+            {
+                Console.Write("[-] Error: bash.exe not found.");
+                return;
+            }
+
+            ProcessStartInfo bashInfo = new ProcessStartInfo();
+            bashInfo.FileName = Path.Combine(bashPath);
+
+            // Loop through args and pass them to git executable
+            StringBuilder argsBld = new StringBuilder();
+
+            argsBld.Append(" --login -c \"git");
+
+            for (int i = 0; i < args.Length; i++)
+            {
+                argsBld.Append(" " + PathConverter.convertPathFromWindowsToLinux(args[i]));
+            }
+
+			argsBld.Append("\"");
+
+			bashInfo.Arguments = argsBld.ToString();
+            bashInfo.UseShellExecute = false;
+            bashInfo.RedirectStandardOutput = true;
+            bashInfo.RedirectStandardError = true;
+            bashInfo.CreateNoWindow = true;
+
+            var proc = new Process
+            {
+                StartInfo = bashInfo
+            };
+
+            proc.OutputDataReceived += CaptureOutput;
+            proc.ErrorDataReceived += CaptureError;
+
+            proc.Start();
+            proc.BeginOutputReadLine();
+            proc.BeginErrorReadLine();
+            proc.WaitForExit();
+        }
+
+        static void CaptureOutput(object sender, DataReceivedEventArgs e)
+        {
+            printOutputData(e.Data);
+        }
+
+        static void CaptureError(object sender, DataReceivedEventArgs e)
+        {
+            printOutputData(e.Data);
+        }
+
+        static void printOutputData(String data)
+        {
+            if (data != null)
+            {
+                if (outputLines > 0)
+                {
+                    Console.Write(Environment.NewLine);
+                }
+                // Print output from git, converting WSL paths to Windows equivalents.
+                Console.Write(PathConverter.convertPathFromLinuxToWindows(data));
+                outputLines++;
+            }
+        }
+    }
+}
